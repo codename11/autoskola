c03cd179c56f80457eef80a0ad1317b45d226293
Veljko Stefanovic <veljkos82@gmail.com>
2016-12-20 23:23:18 +0100

    Juust added a mockup of social network buttons.

--- footer.php
+++ footer.php
@@ -3,17 +3,19 @@ if(!defined('MyConst1')) {
    die('Direct access not permitted');
 }
 ?>
+
 <div class="bkfoot">
+
 	<div class="container-fluid footer">
 
 		<div class="row">
-	<div class="col-sm-3"></div>
-			<div class="col-sm-3">
-				<div class="lt">
-					<p>Auto škola Totem</p>
-					<p>Ulica Lala, 11300 Smederevo</p>
+			<div class="col-sm-3"></div>
+				<div class="col-sm-3">
+					<div class="lt">
+						<p>Auto škola Totem</p>
+						<p>Ulica Lala, 11300 Smederevo</p>
+					</div>
 				</div>
-			</div>
 		
 			
 	
@@ -24,10 +26,18 @@ if(!defined('MyConst1')) {
 					<p class="des">Designed by Lanius™</p>
 				</div>
 			</div>
-		<div class="col-sm-3"></div>
+			<div class="col-sm-3"></div>
 		</div>
 		<div class="row text-center"><a class="up-arrow" id="arrow" href="#up"><span class="glyphicon glyphicon-chevron-up"></span></a></div>
 
+		<div class="row">
+			<div class="col-sm-2"></div>
+			<div class="col-sm-8 text-center">
+				<img src="images/share.jpg" class="tys img-responsive img-rounded" alt="share"><!--Just mockup of social buttons. -->
+			</div>
+			<div class="col-sm-2"></div>
+		</div>
+		
 	</div>
 
 </div>
